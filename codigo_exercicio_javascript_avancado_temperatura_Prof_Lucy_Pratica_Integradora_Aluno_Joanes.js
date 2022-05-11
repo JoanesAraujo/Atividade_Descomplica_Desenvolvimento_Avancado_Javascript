dbfa87a4bc93a53f569b91934d0d6cc64d6da3d6
JoanesAraujo <joanescaraujo@gmail.com>
2022-05-11 18:59:59 -0300

    Praticas Integradoras - Prof Lucy

---
+++ codigo_exercicio_javascript_avancado_temperatura_Prof_Lucy_Pratica_Integradora_Aluno_Joanes.js
@@ -0,0 +1,58 @@
+
+/* Aluno: Joanes Cavalcanti 
+   Turma: Back-End (Descomplica)
+*/
+
+// Calcular media das temperaturas MAX 
+function ArrayAvg(Tmax) {
+    var i = 0, soma = 0, ArrayLen = Tmax.length;
+    while (i < ArrayLen) {
+        soma = soma + Tmax[i++];
+}
+    return soma / ArrayLen;
+}
+
+// Calcular media das temperaturas MIN 
+function ArrayAvg(Tmin) {
+    var i = 0, soma = 0, ArrayLen = Tmin.length;
+    while (i < ArrayLen) {
+        soma = soma + Tmin[i++];
+}
+    return soma / ArrayLen;
+}
+
+
+
+// função para mostrar os dias que choveram
+function choveu(ind){
+    cont = 0;
+    x = 0;
+    do {
+        if(ind[x] == true) {
+            cont++;
+        }
+        x++;
+    } while(x <= ind.length-1);
+    return cont;
+}
+
+// array individual
+dia = [10,2,7,9,3,21,26,22,30,29,5];
+Mes = [1,2,3,4,5,6,7,8,10,12];
+Tmin = [4,0,6,5,7,11,9,12,8,16];
+Tmax = [24,32,36,42,33,39,27,48,22,23];
+ind = [false,true,false,false,true,true,false,true,false,true];
+
+
+
+// -  Agora é só apresentar para o usuário final. - //
+
+// Os dias que choveram
+console.log('\nQuantidade de dias que choveram: ', choveu(ind),'\n');
+// A media da temperatura MAX
+console.log('A média da temperatura MAX: ', ArrayAvg(Tmax),'\n');
+// A media da temperatura MIN
+console.log('A média da temperatura MIN: ', ArrayAvg(Tmin),'\n');
+// A Ordenação da temperatura MAX
+console.log('A ordem das temperaturas MAXIMA: ',Tmax.sort(),'\n');
+
